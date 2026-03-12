f3c3be9ba57d950cbe227ee9edfc69789564129b
Roushan Agarawal <roushanagarawal85@gmail.com>
2026-03-12 07:02:14 +0530

    fix serverless

--- api/index.js
+++ api/index.js
@@ -1,3 +1,7 @@
-import app from "../backend/server.js"
+import app from "../backend/server.js";
+import connectDB from "../backend/config/db.js";
 
-export default app
+export default async function handler(req, res) {
+  await connectDB();   // connect MongoDB
+  return app(req, res); // run Express
+}

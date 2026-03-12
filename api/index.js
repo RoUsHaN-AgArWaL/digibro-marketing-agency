c9f2e55caf0ad4e43375d557fde9de7b7c878f62
Roushan Agarawal <roushanagarawal85@gmail.com>
2026-03-12 06:32:52 +0530

    convert backend to vercel serverless

--- api/index.js
+++ api/index.js
@@ -1,3 +1,8 @@
-import app from "../backend/server.js"
+import connectDB from "../backend/config/db.js";
+import app from "../backend/server.js";
 
-export default app
+export default async function handler(req, res) {
+  await connectDB();
+  return app(req, res);
+}
+
